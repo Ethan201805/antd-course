9fdc46c6e496797ab839f92a2b14a965ce28d62a
Ethan <wxlance2017@gmail.com>
2019-10-24 15:44:33 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,13 @@
+export default {
+    singular: true,
+    routes:[
+        {
+            path: '/',
+            component: './HelloWorld'
+        },
+        {
+            path: '/demo',
+            component: './demo'
+        }
+    ]
+}
